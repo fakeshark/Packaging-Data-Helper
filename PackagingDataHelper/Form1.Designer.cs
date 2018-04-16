04259a8247f4afb7cfb5866f6ff07ca95418f571
Brian-MacDonald-TRG <brian.macdonald@techresearchgroup.com>
2018-04-16 16:31:21 -0400

    Inital

--- PackagingDataHelper/Form1.Designer.cs
+++ PackagingDataHelper/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PackagingDataHelper
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,296 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripNew = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripImport036 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSaveProject = new System.Windows.Forms.ToolStripButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnExportPackaging = new System.Windows.Forms.Button();
+            this.splitPartsListing = new System.Windows.Forms.SplitContainer();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.toolStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitPartsListing)).BeginInit();
+            this.splitPartsListing.Panel1.SuspendLayout();
+            this.splitPartsListing.Panel2.SuspendLayout();
+            this.splitPartsListing.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripNew,
+            this.toolStripButton1,
+            this.toolStripImport036,
+            this.toolStripSaveProject});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(834, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripNew
+            // 
+            this.toolStripNew.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripNew.Image = ((System.Drawing.Image)(resources.GetObject("toolStripNew.Image")));
+            this.toolStripNew.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripNew.Name = "toolStripNew";
+            this.toolStripNew.Size = new System.Drawing.Size(35, 22);
+            this.toolStripNew.Text = "&New";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(40, 22);
+            this.toolStripButton1.Text = "&Open";
+            // 
+            // toolStripImport036
+            // 
+            this.toolStripImport036.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripImport036.Image = ((System.Drawing.Image)(resources.GetObject("toolStripImport036.Image")));
+            this.toolStripImport036.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripImport036.Name = "toolStripImport036";
+            this.toolStripImport036.Size = new System.Drawing.Size(71, 22);
+            this.toolStripImport036.Text = "&Import .036";
+            // 
+            // toolStripSaveProject
+            // 
+            this.toolStripSaveProject.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripSaveProject.Image = ((System.Drawing.Image)(resources.GetObject("toolStripSaveProject.Image")));
+            this.toolStripSaveProject.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripSaveProject.Name = "toolStripSaveProject";
+            this.toolStripSaveProject.Size = new System.Drawing.Size(35, 22);
+            this.toolStripSaveProject.Text = "&Save";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnExportPackaging);
+            this.groupBox1.Controls.Add(this.splitPartsListing);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(834, 536);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Parts Data:";
+            // 
+            // btnExportPackaging
+            // 
+            this.btnExportPackaging.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExportPackaging.Location = new System.Drawing.Point(650, 489);
+            this.btnExportPackaging.Name = "btnExportPackaging";
+            this.btnExportPackaging.Size = new System.Drawing.Size(169, 34);
+            this.btnExportPackaging.TabIndex = 1;
+            this.btnExportPackaging.Text = "Export Packaging Data";
+            this.btnExportPackaging.UseVisualStyleBackColor = true;
+            // 
+            // splitPartsListing
+            // 
+            this.splitPartsListing.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitPartsListing.Location = new System.Drawing.Point(12, 19);
+            this.splitPartsListing.Name = "splitPartsListing";
+            // 
+            // splitPartsListing.Panel1
+            // 
+            this.splitPartsListing.Panel1.Controls.Add(this.listBox1);
+            // 
+            // splitPartsListing.Panel2
+            // 
+            this.splitPartsListing.Panel2.Controls.Add(this.listBox2);
+            this.splitPartsListing.Size = new System.Drawing.Size(810, 464);
+            this.splitPartsListing.SplitterDistance = 418;
+            this.splitPartsListing.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36",
+            "37",
+            "38",
+            "39",
+            "40",
+            "41",
+            "42",
+            "43",
+            "44",
+            "45",
+            "46",
+            "47",
+            "48",
+            "49",
+            "50",
+            "51",
+            "52",
+            "53",
+            "54",
+            "55",
+            "56",
+            "57",
+            "58",
+            "59",
+            "60"});
+            this.listBox1.Location = new System.Drawing.Point(3, 10);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(412, 446);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36",
+            "37",
+            "38",
+            "39",
+            "40",
+            "41",
+            "42",
+            "43",
+            "44",
+            "45",
+            "46",
+            "47",
+            "48",
+            "49",
+            "50",
+            "51",
+            "52",
+            "53",
+            "54",
+            "55",
+            "56",
+            "57",
+            "58",
+            "59",
+            "60"});
+            this.listBox2.Location = new System.Drawing.Point(3, 10);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.ScrollAlwaysVisible = true;
+            this.listBox2.Size = new System.Drawing.Size(382, 446);
+            this.listBox2.TabIndex = 0;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(834, 561);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.MinimumSize = new System.Drawing.Size(850, 600);
+            this.Name = "frmMain";
+            this.Text = "TRG Packaging Data Helper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.splitPartsListing.Panel1.ResumeLayout(false);
+            this.splitPartsListing.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitPartsListing)).EndInit();
+            this.splitPartsListing.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripNew;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripImport036;
+        private System.Windows.Forms.ToolStripButton toolStripSaveProject;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.SplitContainer splitPartsListing;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button btnExportPackaging;
     }
 }
 
--- PackagingDataHelper/Form1.cs
+++ PackagingDataHelper/Form1.cs
@@ -7,14 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
 
 namespace PackagingDataHelper
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        public frmMain()
         {
             InitializeComponent();
+
+            EventHandler handler = (s, e) => {
+                if (s == listBox1)
+                    listBox2.TopIndex = listBox1.TopIndex;
+                if (s == listBox2)
+                    listBox1.TopIndex = listBox2.TopIndex;
+            };
+
+            this.listBox1.MouseCaptureChanged += handler;
+            this.listBox2.MouseCaptureChanged += handler;
+            this.listBox1.SelectedIndexChanged += handler;
+            this.listBox2.SelectedIndexChanged += handler;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
